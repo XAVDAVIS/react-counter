9eabc9f0c0e428bcce07cc1af55a77d0977b3e7a
Xavier <shottyx5156@hotmail.com>
2023-01-06 15:33:39 -0500

    added css on counter file

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,30 @@
+import { useState } from 'react';
+
+
 function Counter() {
+    // the value we pass to the invocation of thr usrState is the inital statre our component needs 
+    // the const state = useState(42); // [42, f]
+
+    // const count = state[0];
+    // const setCount = state[1];
+
+
+    const [count, setCount] = useState(0);
+
+    // Array destructuring assignment syntax
+
+    // when the useState hook is invoked, it returns an array containing the init
+    console.log(count, setCount);
+
     return (
         <>
-            <span>Current Count: 0</span>
+            <span>Current Count: <span style={{color: count < 0 ? 'red' : 'black'}}>{count}</span></span>
             <section>
-                <button>+</button>
-                <button>-</button>
+                <button onClick={() => setCount(count + 1)}>+</button>
+                <button onClick={() => setCount(count - 1)}>-</button>
             </section>
         </>
-    )
+    );
 }
 
-export default Counter;
+export default Counter
